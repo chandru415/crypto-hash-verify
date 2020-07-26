03dd5fd8cf998c54b5374d0eee9ba94a0602393b
chandru415 <chandru9052@gmail.com>
2020-07-26 23:40:07 +0530

    [chandu] added nuget reference & summary

--- CryptoHashVerify/HashVerify.cs
+++ CryptoHashVerify/HashVerify.cs
@@ -6,7 +6,15 @@ namespace CryptoHashVerify
 {
     public static class HashVerify
     {
-
+        /// <summary>
+        /// Generate hash value using Password-Based Key Derivation Function 2
+        /// </summary>
+        /// <param name="targetValue">string - Value to be hash</param>
+        /// <param name="iterationCount">integer - Number of iteratation, default is 1000 </param>
+        /// <param name="algorithm">KeyDerivationPrf - Enum type of KeyDerivationPrf, default is HMACSHA256 </param>
+        /// <param name="salt">byte[] - 128/8 byte code, default is null</param>
+        /// <param name="needsOnlyHash">boolean - condition to return hash</param>
+        /// <returns>type Tuple - (hashed value, salt value)</returns>
         public static (string, string) GenerateHashString(string targetValue, int iterationCount = 1000, KeyDerivationPrf algorithm = KeyDerivationPrf.HMACSHA256, byte[] salt = null, bool needsOnlyHash = false)
         {
             if (salt == null || salt.Length != 16)
@@ -29,7 +37,16 @@ namespace CryptoHashVerify
             return (hashed, Convert.ToBase64String(salt));
         }
 
-
+        /// <summary>
+        /// Verify given hashed value with plan string value after hashing with same settings,
+        /// that was provided on hash
+        /// </summary>
+        /// <param name="hashedValue">string - Encrypted value</param>
+        /// <param name="sourceSalt">string - Based 64 string</param>
+        /// <param name="sourceToCheck">string - plan text</param>
+        /// <param name="iterationCount">integer - Number of iteratation, default is 1000 </param>
+        /// <param name="algorithm">Enum type of KeyDerivationPrf, default is HMACSHA256</param>
+        /// <returns>boolean - return true if comparision success otherwise returns false</returns>
         public static bool VerifyHashString(string hashedValue, string sourceSalt, string sourceToCheck, int iterationCount = 1000, KeyDerivationPrf algorithm = KeyDerivationPrf.HMACSHA256)
         {
             if (hashedValue == null || sourceSalt == null)
